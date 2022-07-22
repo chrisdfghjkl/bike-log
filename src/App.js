fc49638ef3058991cf2d8d0aa0ef3d4b9c89bcc3
chrisdfghjkl <cdepastene@gmail.com>
2022-07-22 13:54:29 -0700

    add routing outline, comment out navbar for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import NavBar from "./components/UI/NavBar";
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <Switch>
+      {/* <NavBar /> */}
+      <Route path='/'>
+
+      </Route>
+      <Route path='/bikes'>
+
+      </Route>
+      <Route path='/bikes/:bikeId'>
+
+      </Route>
+      <Route path='/add-bike'>
+
+      </Route>
+    </Switch>
   );
 }
 
