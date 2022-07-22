0f88d45bde4f7dbcfcd4656681ec8d8020d6d950
chrisdfghjkl <cdepastene@gmail.com>
2022-07-22 13:54:10 -0700

    initial commit + placeholder content

---
+++ src/pages/BikeList.js
@@ -0,0 +1,9 @@
+const BikeList = () => {
+  return (
+    <div>
+      <h1>My Bikes</h1>
+    </div>
+  )
+};
+
+export default BikeList;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to bike log</h1>
+      <h3>Let's get started</h3>
+    </div>
+  )
+};
+
+export default Home;
