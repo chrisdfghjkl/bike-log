b8490a74582cfd8a6c94fec1f4808f7bc0646c86
chrisdfghjkl <cdepastene@gmail.com>
2022-07-22 20:05:01 -0700

    Add log section

--- src/components/log/Log.js
+++ src/components/log/Log.js
@@ -4,7 +4,24 @@ import styles from './Log.module.css';
 
 
 const Log = () => {
-  return
+  const [isAddingLogItem, setIsAddingLogItem] = useState(false);
+
+  const startAddLogItemHandler = () => {
+    setIsAddingLogItem(true);
+  };
+
+  return (
+    <section className={styles.log}>
+      <h2>Log</h2>
+      {!isAddingLogItem && (
+        <button className='btn btn-warning' onClick={startAddLogItemHandler}>
+          Add Log Entry
+        </button>
+      )}
+      {/* {isAddingLogItem && <NewLogForm />} */}
+      <p>Log...</p>
+    </section>
+  )
 };
 
 export default Log;
