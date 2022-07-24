9be2ff84a1d9d7f329b68cfceb2801df64b5495d
chrisdfghjkl <cdepastene@gmail.com>
2022-07-24 14:58:39 -0700

    add list of bike component content

---
+++ src/components/bikes/AllBikes.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+
+import BikeItem from "./BikeItem";
+import styles from "./AllBikes.module.css";
+
+const AllBikes = (props) => {
+  return (
+    <Fragment>
+      <ul className={styles.list}>
+        {props.bikes.map((bike) => (
+          <BikeItem
+            key={bike.id}
+            id={bike.id}
+            make={bike.make}
+            model={bike.model}
+            year={bike.model}
+            style={bike.style}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  )
+};
