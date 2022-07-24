48d843a798e6ffe3423456b3464250951277fff9
chrisdfghjkl <cdepastene@gmail.com>
2022-07-24 11:42:07 -0700

    add import statements + props

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,13 @@
+import { Fragment } from 'react';
+import Navbar from './Navbar';
+
 import styles from './Layout.module.css';
 
-const Layout = () => {
-
+const Layout = (props) => {
+  return <Fragment>
+    <Navbar />
+    <main className={styles.layout}>{props.children}</main>
+  </Fragment>
 };
 
 export default Layout;
