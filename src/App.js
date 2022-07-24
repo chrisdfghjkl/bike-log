bb3080ed76e5d1286a7c4d4e03dfff242b4839cc
chrisdfghjkl <cdepastene@gmail.com>
2022-07-24 11:42:19 -0700

    add layout wrapper

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import Home from "./pages/Home";
 import BikeDetails from "./pages/BikeDetails";
 import BikeList from "./pages/BikeList";
 import AddBike from "./pages/AddBike";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/home' />
-      </Route>
-      <Route path='/home'>
-        <Home />
-      </Route>
-      <Route path='/bikes' exact>
-        <BikeList />
-      </Route>
-      <Route path='/bikes/:bikeId'>
-        <BikeDetails />
-      </Route>
-      <Route path='/add-bike'>
-        <AddBike />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home'>
+          <Home />
+        </Route>
+        <Route path='/bikes' exact>
+          <BikeList />
+        </Route>
+        <Route path='/bikes/:bikeId'>
+          <BikeDetails />
+        </Route>
+        <Route path='/add-bike'>
+          <AddBike />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
