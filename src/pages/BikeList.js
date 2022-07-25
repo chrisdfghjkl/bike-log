5b3ebcfc3bbfe20bfea4d92f02351eaaaea44c8d
chrisdfghjkl <cdepastene@gmail.com>
2022-07-25 13:29:46 -0700

    add semicolon

--- src/pages/BikeList.js
+++ src/pages/BikeList.js
@@ -3,7 +3,7 @@ import AllBikes from "../components/bikes/AllBikes";
 const SEED_BIKES = [
   { id: 'b1', make: 'Poseidon', model: 'Flatbar X', year: '2021', style: 'Gravel' },
   { id: 'b2', make: 'Kona', model: 'Dew Plus', year: '2020', style: 'Commuter' }
-]
+];
 
 const BikeList = () => {
   return <AllBikes bikes={SEED_BIKES} />
