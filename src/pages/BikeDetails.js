99238a03a699c754f4cfe811e99c73e27514d173
chrisdfghjkl <cdepastene@gmail.com>
2022-07-25 13:19:10 -0700

    add seed bike data to test details page

--- src/pages/BikeDetails.js
+++ src/pages/BikeDetails.js
@@ -3,8 +3,15 @@ import { useParams, Route } from "react-router-dom";
 
 import Log from "../components/log/Log";
 
+const SEED_BIKES = [
+  { id: 'b1', make: 'Poseidon', model: 'Flatbar X', year: '2021', style: 'Gravel' },
+  { id: 'b2', make: 'Kona', model: 'Dew Plus', year: '2020', style: 'Commuter' }
+];
+
 const BikeDetails = () => {
   const params = useParams();
+
+  const bike = SEED_BIKES.find(bike => bike.id === params.bikeId);
 
   return ( 
     <Fragment>
