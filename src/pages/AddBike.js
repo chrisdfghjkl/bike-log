52a61b6069567843c47928de4709590970a782fe
chrisdfghjkl <cdepastene@gmail.com>
2022-07-27 20:49:46 -0700

    connect add bike to database

--- src/pages/AddBike.js
+++ src/pages/AddBike.js
@@ -1,18 +1,26 @@
 import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import useHttp from "../hooks/use-http";
+import { addBike } from "../lib/api";
 
 import BikeForm from "../components/bikes/BikeForm";
 
 const AddBike = () => {
+  const { sendRequest, status } = useHttp(addBike);
   const history = useHistory();
 
-  const addBikeHandler = (bikeData) => {
-    console.log(bikeData);
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/bikes')
+    }
+  }, [status, history]);
 
-    history.push('/bikes')
+  const addBikeHandler = (bikeData) => {
+    sendRequest(bikeData);
   };
 
 
-  return <BikeForm onAddBike={addBikeHandler} />
+  return <BikeForm isLoading={status === 'pending'} onAddBike={addBikeHandler} />
 };
 
 export default AddBike;
