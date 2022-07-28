2843749cc5e1198ed9e18d339205a8426b6e8f26
chrisdfghjkl <cdepastene@gmail.com>
2022-07-27 20:47:13 -0700

    update all props for seed bike test

--- src/pages/BikeDetails.js
+++ src/pages/BikeDetails.js
@@ -5,7 +5,7 @@ import ExpandedBike from "../components/bikes/ExpandedBike";
 import Log from "../components/log/Log";
 
 const SEED_BIKES = [
-  { id: 'b1', make: 'Poseidon', model: 'Flatbar X', year: '2021', style: 'Gravel' },
+  { id: 'b1', make: 'Poseidon', model: 'Flatbar X', year: '2021', style: 'Gravel', frame: '6061 Double Butted Hydroformed Aluminum', size: 'LG', fork: 'Full Carbon fork', fDerailleur: '', rDerailleur: 'MicroShift Advent X 10 Speed with Clutch', bar: 'Poseidon Flatbars', barGrip: 'Ergon-GP3', shifters: 'MicroShift Advent X 10 Speed', cassette: 'Microshift 11-48T Advent X 10 Speed', chain: 'KMC 10 Speed', crank: 'Prowheel 38t Narrow Wide Chainring with 170mm crank arms', brakes: 'Tektro MD-C510 Mechanical Disc', levers: 'Tektro Flatbar Levers', pedals: 'RaceFace Ride Pedal', saddle: 'Poseidon X (stock)', tires: 'Kenda Small Block 700x35', wheels: 'Poseidon Alloy 700c 32h, quick-release axle', frontLight: 'Planet Bike Blaze 150SL', rearLight: 'Planet Bike Superflash 65R', fenders: 'PDW Oragami Fenders', rack: '', basket: 'PDW Bodega Basket', Bell: '' },
   { id: 'b2', make: 'Kona', model: 'Dew Plus', year: '2020', style: 'Commuter' }
 ];
 
