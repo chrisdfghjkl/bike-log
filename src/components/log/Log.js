9c9b8d4ebaaf3e2a47959c461ba7c1bfa563c219
chrisdfghjkl <cdepastene@gmail.com>
2022-07-28 16:31:52 -0700

    begin add functionality

--- src/components/log/Log.js
+++ src/components/log/Log.js
@@ -1,27 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from './Log.module.css';
 import NewLogForm from "./NewLogForm";
 import Card from "../UI/Card";
+import { useParams } from "react-router-dom";
 
+import useHttp from "../../hooks/use-http";
+import { getAllLogs } from "../../lib/api";
+import LoadingWheel from "../UI/LoadingWheel";
 
 const Log = () => {
   const [isAddingLogItem, setIsAddingLogItem] = useState(false);
+  const params = useParams();
+
+  const { bikeId } = params;
+
+  const { sendRequest, status, data: loadedLogs } =useHttp(getAllLogs);
+
+  useEffect(() => {
+    sendRequest(bikeId);
+  }, [bikeId, sendRequest]);
 
   const startAddLogItemHandler = () => {
     setIsAddingLogItem(true);
   };
 
+  const addedLogHandler = () => {};
+
+  let logs;
+
+  if (status === 'pending') {
+    logs = (
+      <div className="centered">
+        <LoadingWheel />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && loadedLogs) {
+    
+  }
+
   return (
     <Card>
     <section className={styles.log}>
       {!isAddingLogItem && (
-        <div className="btn-group">
-          <button className='btn' onClick={startAddLogItemHandler}>Log Ride</button>
-          <button className='btn' onClick={startAddLogItemHandler}>Log Service</button>
-        </div>
+        <button className='btn' onClick={startAddLogItemHandler}>Log Service</button>
       )}
-      {isAddingLogItem && <NewLogForm />}
+      {isAddingLogItem && <NewLogForm bikeId={params.bikeId} onAddedLog={addedLogHandler}/>}
     </section>
     </Card>
   )
