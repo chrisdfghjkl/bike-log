b7954493199ead189d5c569e4c99b1af31b11388
chrisdfghjkl <cdepastene@gmail.com>
2022-07-30 11:32:05 -0700

    import card for no bike page styling

--- src/components/bikes/NoBikes.js
+++ src/components/bikes/NoBikes.js
@@ -1,14 +1,17 @@
 import styles from './NoBikes.module.css';
 import { Link } from 'react-router-dom';
+import Card from '../UI/Card'
 
 const NoBikes = () => {
   return (
+    <Card>
     <div className={styles.nobikes}>
       <p>No bikes found!</p>
       <Link className='btn' to='/add-bike'>
         Add a Bike
       </Link>
     </div>
+    </Card>
   );
 };
 
